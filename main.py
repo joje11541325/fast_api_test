a1cd70d9f5369ed7a29dda9fcd0ef78ff301e74f
Jonathan <jhedenrud02@gmail.com>
2025-08-16 18:08:29 +0200

    toy example api

--- main.py
+++ main.py
@@ -1,43 +1,38 @@
 """This api"""
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query, HTTPException, status
 from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-items = {1: {"name": "eggs", "price": 4.44},
-         2: {"name": "chicken", "price": 100.44}}
-
-
-@app.get("/")
-def firstpage():
-    return {"Where you are": "Second page man."}
-
-
-@app.get("/om oss")
-def aboutus():
-    return {"Info": """Vi är ett företag med kärlek för det äkta
-    Vi tar inget för givet
-    Ge mig lite"""}
-
-
-items = {1: {"name": "eggs", "price": 4.44},
-         2: {"name": "chicken", "price": 100.44}}
-
-
-@app.get("/search_item")
-def get_item(name: str = None):
-    for item in items.values():
-        if item["name"] == name:
-            return item
-    return "no items found"
+emails = {}
 
 
 class Email(BaseModel):
-    subject: str
-    body: str
-    email: str
+    """give more"""
+    subject: Optional[str] = None
+    body: Optional[str] = None
+    email: Optional[str] = None
 
 
-@app.post("/draft-email")
-def draft_email(email: Email = None):
-    return {"data": "you uploaded an email"}
+@app.post("/draft-email/{email_id}")
+def draft_email(email_id: int, email: Email = None):
+    emails[email_id] = email
+    return emails[email_id]
+
+
+@app.get("/find-email/{email_id}")
+def find_email(email_id: int = Path(description="Here du pass the id of the email you want to find")):
+    if email_id in emails:
+        return emails[email_id].subject
+    return "No email with that id!"
+
+
+@app.put("/edit-email/{email_id}")
+def edit_email(email_id: int, email: Email):
+    if email_id in emails:
+        emails[email_id] = email
+        return emails[email_id]
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="No item with that id")
